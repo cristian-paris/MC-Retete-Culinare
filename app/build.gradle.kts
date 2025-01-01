fad48c2372f9362a1367da806b83f3d838c355a6
Robert Maracine <mmaracine@adobe.com>
2025-01-01 17:03:19 +0200

    Upgrade compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.mc_retete_culinare"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mc_retete_culinare"
